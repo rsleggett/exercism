e06646440747638e60fd9a01677d30e0525ad347
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-01-07 12:56:21 +0100

    TwoFer v2

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -3,6 +3,8 @@
 open System
 
 let name (input: string option): string = 
-    match input with
-        | None -> "One for you, one for me."
-        | Some input -> "One for " + input + ", one for me."
+    let person =
+        match input with
+            | None -> "you"
+            | Some s -> s  
+    sprintf "One for %s, one for me." person
