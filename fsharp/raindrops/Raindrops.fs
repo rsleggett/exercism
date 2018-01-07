072a22a1007b81ad896fec6ad276e6b2f5d07557
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-01-07 15:46:25 +0100

    Raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,15 @@
+﻿module Raindrops
+
+// - If the number has 3 as a factor, output 'Pling'.
+// - If the number has 5 as a factor, output 'Plang'.
+// - If the number has 7 as a factor, output 'Plong'.
+// - If the number does not have 3, 5, or 7 as a factor,
+//   just pass the number's digits straight through.
+
+let convert (number: int): string = 
+    let hasFactor (number: int) (factor: int): bool = number % factor = 0
+    match number with
+    | hasFactor number 3 -> "Pling"
+    | hasFactor number 5 -> "Plang"
+    | hasFactor number 7 -> "Plong"
+    | _ -> number
