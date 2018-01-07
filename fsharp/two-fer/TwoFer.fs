5da710915962c565dd3aafa37ed26857a0beee2a
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-01-07 12:53:08 +0100

    Two Fer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,8 @@
+﻿module TwoFer
+
+open System
+
+let name (input: string option): string = 
+    match input with
+        | None -> "One for you, one for me."
+        | Some input -> "One for " + input + ", one for me."
