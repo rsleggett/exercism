ff669ee2d6c44bab991276ad2796b22a0b51df49
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-01-08 22:16:47 +0100

    Accumulate

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,6 @@
+﻿module Accumulate
+
+let rec accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
+    match input with
+    | [] -> []
+    | head::tail -> [func head] @ accumulate func tail
