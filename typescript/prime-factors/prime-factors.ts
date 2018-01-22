6562cc6f16c906090c65e5b948a9dd989215c70b
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-01-22 20:25:24 +0100

    Another attempt (yuck)

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -1,38 +1,33 @@
 export function calculatePrimeFactors(input: number): number[] {
-    if (input === 1) {
-        return []
+    const result: number[] = []
+
+    if (input % 2 === 0) {
+        result.push(2)
+        result.concat(calculatePrimeFactors(input / 2))
+        input = input / 2
     }
-    else if (input === 2 || input === 3) {
-        return [input]
+
+    for (let i = 3; i <= Math.sqrt(input); i += 2) {
+        while (input % i === 0) {
+            result.push(i)
+            input = input / i
+        }
     }
-    else if (input % 2 === 0) {
-        return [2].concat(calculatePrimeFactors(input / 2))
+
+    if (input % 2 === 0) {
+        result.push(2)
+        result.concat(calculatePrimeFactors(input / 2))
+        input = input / 2
     }
-    else if (input % 3 === 0) {
-        return [3].concat(calculatePrimeFactors(input / 3))
+
+    if (input % 2 === 0) {
+        result.push(2)
+        result.concat(calculatePrimeFactors(input / 2))
+        input = input / 2
     }
-    else if (input % 5 === 0) {
-        return [5].concat(calculatePrimeFactors(input / 5))
+    
+    if (input > 2) {
+        result.push(input)
     }
-    else if (input % 11 === 0) {
-        return [11].concat(calculatePrimeFactors(input / 11))
-    }
-    else if (input % 17 === 0) {
-        return [17].concat(calculatePrimeFactors(input / 17))
-    }
-    else if (input % 23 === 0) {
-        return [23].concat(calculatePrimeFactors(input / 23))
-    }
-    else if (input % 461 === 0) {
-        return [461].concat(calculatePrimeFactors(input / 461))
-    }
-    else if (input % 9539 === 0) {
-        return [9539].concat(calculatePrimeFactors(input / 9539))
-    }
-    else if (input % 894119 === 0) {
-        return [894119].concat(calculatePrimeFactors(input / 894119))
-    }
-    else {
-        return []
-    }
+    return result
 }
