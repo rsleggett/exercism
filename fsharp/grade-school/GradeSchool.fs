6acf85c133d167bd94960804bbfd77a24e5f4c9b
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-01-28 20:56:25 +0100

    Add solution for GradeSchool

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -1,9 +1,23 @@
 ﻿module GradeSchool
 
-let empty: Map<int, string list> = failwith "You need to implement this function."
+let empty: Map<int, string list> = 
+    Map.empty
 
-let add (student: string) (grade: int) (school: Map<int, string list>): Map<int, string list> = failwith "You need to implement this function."
+let add (student: string) (gradeNumber: int) (school: Map<int, string list>): Map<int, string list> = 
+    if school.ContainsKey(gradeNumber) then school.Add(gradeNumber, school.[gradeNumber] @ [student])
+    else school.Add(gradeNumber, [student])
 
-let roster (school: Map<int, string list>): (int * string list) list = failwith "You need to implement this function."
+let grade (number: int) (school: Map<int, string list>): string list = 
+    if(school.ContainsKey(number)) then
+        school.[number]
+        |> List.sort
+    else []
 
-let grade (number: int) (school: Map<int, string list>): string list = failwith "You need to implement this function."
+let roster (school: Map<int, string list>): (int * string list) list = 
+    let mutable gradeList = []
+    let sorted = school |> Map.toSeq |> Seq.map fst
+    
+    for gradeNumber in sorted do
+        gradeList <- gradeList @ [(gradeNumber, grade gradeNumber school)]
+    gradeList
+
