c426196d4051501462c22c4c4d3311bf3c9cf3f2
Robert Stevenson-Leggett <rsleggett@googlemail.com>
2018-02-11 13:11:42 +0100

    Armstrong numbers

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,14 @@
+﻿module ArmstrongNumbers
+
+open System
+
+let isArmstrongNumber (number: int): bool = 
+    let numbers = Convert.ToString(number).ToCharArray() |> Array.toList
+    let length: double = numbers.Length |> double
+
+    let value = numbers  
+                |> List.map (fun x -> (System.Char.GetNumericValue(x) |> double) ** length) 
+                |> List.sum
+
+    Console.WriteLine(value)
+    value = (number |> double)
